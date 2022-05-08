6e87e2f367829d4ac61c2276ac5fe802ac995e90
Mario <lombardomario01@gmail.com>
2022-05-08 17:18:26 +0200

    Header component created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-      <p>ciao sono Header</p>
-  </div>
+    <header>
+        <nav>
+            <section id="nav-left">
+                <img src="../assets/logo.png" alt="logo">
+                <ul>
+                    <li>
+                        <a href="#">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Serie TV
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Film
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nuovi e popolari
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            La mia lista
+                        </a>
+                    </li>
+                </ul>
+            </section>
+
+            <section id="nav-right">
+
+            </section>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -18,5 +53,67 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+header {
+    width: 100%;
+    height: 68px;
+    background-image: linear-gradient(to bottom,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
+    font-family: 'Netflix Sans','Helvetica Neue',Helvetica,Arial,sans-serif;
+
+    nav {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 60px;
+
+        section {
+            display: flex;
+            align-items: center;
+        }
+
+        section#nav-left {
+            width: 70%;
+
+            img {
+                width: 8%;
+                margin-right: 35px;
+            }
+
+            ul {
+                display: flex;
+                list-style: none;
+
+                li {
+                    margin-right: 17px;
+                    font-size: 14px;
+                    font-weight: 700;
+
+                    &:first-child {
+
+                        a {
+                            color: #fff;
+                        }
+                    }
+
+                    a {
+                        text-decoration: none;
+                        color: #dedede;
+                    }
+                }
+            }
+        }
+
+        section#nav-right {
+            width: 30%;
+            // debug
+            background-color: red;
+        }
+    }
+
+}
+
+
 
 </style>
