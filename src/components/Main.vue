1406c96390fa925f0ce01a7361e022d363730ae2
Mario <lombardomario01@gmail.com>
2022-05-08 18:12:17 +0200

    Add Jumbotron

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-      <p>Ciao sono Main</p>
-  </div>
+  <main>
+    <!-- Jumbo -->
+    <div class="jumbotron">
+        <img src="../assets/test.png" alt="test">
+    </div>
+  </main>
 </template>
 
 <script>
@@ -19,4 +22,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
+main {
+    div.jumbotron {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        opacity: 1;
+        transition: opacity .4s cubic-bezier(.665,.235,.265,.8) 0s;
+
+        img {
+            width: 100%;
+        }
+    }
+}
 </style>
