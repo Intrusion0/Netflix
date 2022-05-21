1637882747ad92fed20003e4f621326452d132cd
Andreea1696 <andreeapetrescu16@gmail.com>
2022-05-21 20:16:20 +0200

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -30,5 +30,36 @@ export default {
   box-sizing: border-box;
 }
 
+section#populars, section#tops, section#top10 {
+  position: relative;
+  padding: 28px 0;
+}
+
+h2 {
+  color: #e5e5e5;
+  margin-bottom: 15px;
+  font-weight: 700;
+  font-size: 32px;
+}
+
+.row {
+  display: flex;
+
+  .card {
+    height: 200px;
+    width: 325px;
+    min-width: 325px;
+    border-radius: 4px;
+    margin-right: 8px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 4px;
+    }
+  }
+}
+
 
 </style>
